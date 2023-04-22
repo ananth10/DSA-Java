2dabfcd56de7c49447c8c9e6bd5e63f16bb39cbc
ananth <ananthababu.v@gmail.com>
2023-04-22 16:49:16 +0530

    Completed Stack Data Structure.

---
+++ app/src/main/java/com/example/myapplication/datastructures/array/codingminutes/stack/using_linked_list/Stack.java
@@ -0,0 +1,41 @@
+package com.example.myapplication.datastructures.array.codingminutes.stack.using_linked_list;
+
+
+public class Stack<T> {
+
+    private Node<T> head;
+
+    public Stack() {
+        this.head = null;
+    }
+
+    public class Node<T> {
+        T data;
+        Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void push(T data) {
+        Node<T> node = new Node<>(data);
+        node.next = head;
+        head = node;
+    }
+
+    public void pop() {
+        if (head != null) {
+            head = head.next;
+        }
+    }
+
+   public T getTop() {
+        return head.data;
+    }
+
+    public boolean empty() {
+        return head == null;
+    }
+}
