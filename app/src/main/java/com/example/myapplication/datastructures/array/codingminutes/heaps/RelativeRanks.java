916221128bbcf9d8a8c86ca424f180df0a5bcbb7
ananth <ananthababu.v@gmail.com>
2023-05-21 15:54:45 +0530

    Completed RelativeRanks problem

---
+++ app/src/main/java/com/example/myapplication/datastructures/array/codingminutes/heaps/RelativeRanks.java
@@ -0,0 +1,35 @@
+package com.example.myapplication.datastructures.array.codingminutes.heaps;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class RelativeRanks {
+
+    public static void main(String[] args) {
+        int[] scores = {4, 3, 6, 8, 1, 9};
+        String[] ranks = relativeRanks(scores);
+        System.out.println(Arrays.toString(ranks));
+    }
+
+    static String[] relativeRanks(int[] scores) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int score : scores) {
+            queue.add(score);
+        }
+        int position = 1;
+        String[] ranks = new String[scores.length];
+        String[] medals = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+        while (!queue.isEmpty()) {
+            if (position <= 3) {
+                ranks[position - 1] = medals[position - 1];
+            } else {
+                ranks[position - 1] = String.valueOf(position);
+            }
+            queue.poll();
+            position++;
+        }
+        return ranks;
+    }
+}
