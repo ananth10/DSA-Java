981a2c3e191c6ceaef205bb75eaf8ba5a5508e03
Ananth10 <ananthababu.v@gmail.com>
2024-02-22 11:53:58 +0530

    1. VertexCover dfs and bfs - problems done .

--- app/src/main/java/com/example/myapplication/dynamic_programming/tree/VertexCover.java
+++ app/src/main/java/com/example/myapplication/dynamic_programming/tree/VertexCover.java
@@ -1,15 +1,15 @@
 package com.example.myapplication.dynamic_programming.tree;
 
-import com.example.myapplication.codingminutes.graph.AdjacencyList01;
-
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Vector;
+import java.util.LinkedList;
+import java.util.Queue;
 
 
 public class VertexCover {
     static Graph graph;
     static int[][] dp;
+
+    static int root;
 
     public static void main(String[] args) {
         int n = 8;
@@ -25,6 +25,15 @@ public class VertexCover {
         int result = Math.min(vertexCover(1, true, -1), vertexCover(1, false, -1));
 
         System.out.println("Result: " + result);
+
+        dp = new int[n][2];
+        dfs(1, 0);
+        int result1 = Math.min(dp[1][0], dp[1][1]);
+        System.out.println("DFS Result1: " + result1);
+
+        dp = new int[n][2];
+        int result2 = bfs();
+        System.out.println("BFS Result1: " + result2);
     }
 
     static int vertexCover(int current, boolean take, int parent) {
@@ -42,6 +51,53 @@ public class VertexCover {
         return dp[current][_take];
     }
 
+
+    static void dfs(int current, int parent) {
+        dp[current][0] = 0;
+        dp[current][1] = 1;
+
+        for (int x : graph.list.get(current)) {
+            if (x != parent) {
+                dfs(x, current);
+
+                dp[current][0] += dp[x][1];
+                dp[current][1] += Math.min(dp[x][0], dp[x][1]);
+            }
+        }
+    }
+
+
+    static int bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[graph.list.size()];
+        for (int i = 0; i < graph.list.size(); i++) {
+            if (graph.list.get(i).size() == 1) {
+                queue.add(i);
+            }
+        }
+        for (int j : queue) {
+            System.out.println("EL:" + j);
+        }
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            visited[current] = true;
+            root = current;
+            dp[current][0] = 0;
+            dp[current][1] = 1;
+
+            for (int x : graph.list.get(current)) {
+                if (visited[x]) {
+                    dp[current][0] += dp[x][1];
+                    dp[current][1] += Math.min(dp[x][0], dp[x][1]);
+                } else {
+                    queue.add(x);
+                }
+            }
+
+        }
+
+        return Math.min(dp[root][0], dp[root][1]);
+    }
 
     private static class Graph {
         int V;
