a3ed5d7136b651259a00384838f6991ce52c248e
ananth <ananthababu.v@gmail.com>
2024-03-02 15:45:17 +0530

    lcs bottomup

--- app/src/main/java/com/example/myapplication/dynamic_programming/strings/LongestCommonSubSequence.java
+++ app/src/main/java/com/example/myapplication/dynamic_programming/strings/LongestCommonSubSequence.java
@@ -16,6 +16,9 @@ public class LongestCommonSubSequence {
         int result = lcs(0, 0, dp);
 
         System.out.println("result topDown:" + result);
+
+        int result1 = lcsBtm();
+        System.out.println("result bottomUp:" + result1);
     }
 
     //TopDown
@@ -42,13 +45,28 @@ public class LongestCommonSubSequence {
     }
 
     //lcs
-//    static int lcsBtm(){
-//        int[][] dp = new int[100][100];
-//
-//        for (int[] row : dp) {
-//            Arrays.fill(row, -1);
-//        }
-//
-//
-//    }
+    static int lcsBtm() {
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+
+        if (a.length() == 0 && b.length() == 0)
+            return 0;
+
+        for (int i = 0; i <= a.length(); i++) {
+            for (int j = 0; j <= b.length(); j++) {
+
+                //if a string is empty
+                if (i == 0) {
+                    dp[0][j] = 0;
+                } else if (j == 0) { //if b string is empty
+                    dp[i][0] = 0;
+                } else if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[a.length()][b.length()];
+    }
 }
