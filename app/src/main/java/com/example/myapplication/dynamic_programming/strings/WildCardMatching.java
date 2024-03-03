137d58158481414c530cee94c11500af8f74b354
Ananth10 <ananthababu.v@gmail.com>
2024-03-03 12:11:16 +0530

    1. wildcard btm - problems done .

--- app/src/main/java/com/example/myapplication/dynamic_programming/strings/WildCardMatching.java
+++ app/src/main/java/com/example/myapplication/dynamic_programming/strings/WildCardMatching.java
@@ -16,6 +16,9 @@ public class WildCardMatching {
         int result = wildMatch(0, 0, dp);
 
         System.out.println("Result TopDown :" + (result == 1));
+
+        int result1 = wildMatchBtmUp();
+        System.out.println("Result BottomUp :" + (result1 == 1));
     }
 
     //TopDown approach
@@ -46,5 +49,45 @@ public class WildCardMatching {
         }
 
         return dp[i][j] = 0;
+    }
+
+    //bottom up
+    static int wildMatchBtmUp() {
+        int[][] dp = new int[s.length() + 1][p.length() + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, 0);
+        }
+        boolean makeAll = false;
+        //base case
+        dp[0][0] = 1; //both string and pattern are empty
+
+        //if pattern is empty
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][0] = 0;
+        }
+        //if string is empty
+        for (int k = 0; k < p.length(); k++) {
+            if (p.charAt(0) == '*') {
+                dp[0][k] = 1;
+                makeAll = true;
+            } else {
+                if (makeAll)
+                    dp[0][k] = 1;
+
+            }
+        }
+
+        for (int i = 1; i < s.length(); i++) {
+            for (int j = 1; j < p.length(); j++) {
+
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 1] | dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[s.length()][p.length()];
     }
 }
